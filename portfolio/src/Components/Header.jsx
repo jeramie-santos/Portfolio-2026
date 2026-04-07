89a89d9aa0b553fe2257a0c67ab743652b80293c
Jeramie <jeramieramsantos79@gmail.com>
2026-04-07 12:23:05 +0800

    installed tailwind, fontawesome and created header component

---
+++ portfolio/src/Components/Header.jsx
@@ -0,0 +1,28 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars } from '@fortawesome/free-solid-svg-icons'
+
+const Header = () => {
+    return (
+        <header className="border border-black py-10 px-5">
+            <nav className="flex justify-between items-center">
+                <div>
+                    <p className="text-3xl font-bold tracking-widest">JERAMIE</p>
+                </div>
+                <div className="hidden">
+                    <ul>
+                        <li><a href="#">About</a> Me</li>
+                        <li><a href="#">Projects</a></li>
+                        <li><a href="#">Work Experience</a></li>
+                        <li><a href="#">Education</a></li>
+                        <li><a href="#">Contact</a></li>
+                    </ul>
+                </div>
+                <div className="block">
+                    <FontAwesomeIcon icon={faBars} className='text-3xl hover:cursor-pointer'/>
+                </div>
+            </nav>
+        </header>
+    )
+}
+
+export default Header
